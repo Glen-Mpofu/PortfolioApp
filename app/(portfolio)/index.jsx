d0f72450e9c52d6af1808beaa0691543d21cbf0b
Tshepo <mgthades2004@gmail.com>
2025-08-19 09:20:57 +0200

    Updated Portfolio page

--- app/(portfolio)/index.jsx
+++ app/(portfolio)/index.jsx
@@ -1,31 +1,50 @@
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import SocialIcons from 'react-native-vector-icons/FontAwesome5';
 
 const app = () => {
   return (
     <View styles={styles.container}>
+      
       <Text style={styles.heading}>Porfolio</Text>
+      
+      <View>
+        <View style={styles.row}>
+          <Image source={require('../../assets/images/tshepo.jpg')} style={styles.image} />
 
-      <View style={styles.row}>
-        <Image source={require('../../assets/images/tshepo.jpg')} style={styles.image} />
+          <View style={styles.column}>
+            <Text style={styles.regText}>Tshepo</Text>
+            <Text style={styles.regText}>21</Text>
+            <Text style={styles.regText}>Black South African</Text>
+          </View>
 
-        <View style={styles.column}>
-          <Text style={styles.regText}>Tshepo</Text>
-          <Text style={styles.regText}>21</Text>
-          <Text style={styles.regText}>Black South African</Text>
         </View>
-
       </View>
 
-      <Text style={styles.regText}>
-        Born and raised in Hillbrow South Africa I've learned to be creative in everything that i do. I don't contribute my energy to projects i don't see as creative.
+      <View style = {styles.card}>
+        <Text style={styles.regText}>
+          Born and raised in Hillbrow South Africa I've learned to be creative in everything that i do. I don't contribute my energy to projects i don't see as creative.
 
-        Speaking of creativity, i make music :) and i think that side of moi helps me tap into more outlandish or eclectic programming project ideas.
+          Speaking of creativity, i make music :) and i think that side of moi helps me tap into more outlandish or eclectic programming project ideas.
 
-        Gosh!! i know you resent long texts so enough chatter, look no further than <Link href="/skills">here</Link>
-      </Text>
+          Gosh!! i know you resent long texts so enough chatter, look no further than below for my projects and social links
+        </Text>
+      </View>
 
+      <View style = {styles.linksRow}>
+        <Link href={'https://www.linkedin.com/in/tshepo-mpofu-6b37a3237/'}>
+          <SocialIcons name = "linkedin" size = {30} color = "blue" style = {styles.icon}/>   
+        </Link>
+
+        <Link href={'https://www.instagram.com/i.am.mgt/'}>
+          <SocialIcons name="instagram" size={30} color="#C13584" style={styles.icon} />
+        </Link>
+
+        <Link href={'https://github.com/Glen-Mpofu'}>
+          <SocialIcons name = "github" size={30} color="black" style ={styles.icon}/>
+        </Link>
+      </View>
     </View>
   )
 }
@@ -33,8 +52,7 @@ const app = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'brown',
+    flexDirection: 'column'
   },
   heading: {
     fontFamily: 'ChelaOne',
@@ -46,10 +64,13 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
+    alignContent: 'center',
+
   },
   row: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row', 
+    alignSelf: 'center'
   },
   column: {
     paddingTop: 20,
@@ -58,6 +79,23 @@ const styles = StyleSheet.create({
   },
   regText: {
     fontFamily: 'PTSans',
-  }
+    textAlign: 'center',
+  },
+  card:{
+    height: 150,
+    width: 300,
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'brown',
+  }, 
+  linksRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'center'
+  },
+  icon: {
+    marginHorizontal: 5,
+  },
 })
 export default app
